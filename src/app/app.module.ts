25d6adb92578739d6ed5e239a5b88784f83d53bc
Aleksandr Serenko <fafnur@yandex.ru>
2021-04-23 00:55:03 +0700

    Integrate room libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { RootStoreModule } from '@app/core/store/root';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppLocaleModule } from './app-locale.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppLocaleModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppLocaleModule,
+    HttpClientModule,
+    RootStoreModule,
+    !environment.production ? StoreDevtoolsModule.instrument({ logOnly: environment.production }) : [],
+  ],
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
