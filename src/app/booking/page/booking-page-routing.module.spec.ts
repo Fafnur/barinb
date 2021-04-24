931dec722b790334b41657368bf71c0379590604
Aleksandr Serenko <fafnur@yandex.ru>
2021-04-24 15:34:56 +0700

    Fix BookingPageRoutingModule tests

--- src/app/booking/page/booking-page-routing.module.spec.ts
+++ src/app/booking/page/booking-page-routing.module.spec.ts
@@ -1,11 +1,18 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
+import { BookingPageComponent } from './booking-page.component';
 import { BookingPageRoutingModule } from './booking-page-routing.module';
 
 describe('BookingPageRoutingModule', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [BookingPageRoutingModule],
+      declarations: [
+        // TODO: Fix component BookingPageComponent is not part of any NgModule or the module has not been imported into your module.
+        BookingPageComponent
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
