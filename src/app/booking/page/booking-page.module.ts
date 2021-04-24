92b5ed43ffa5caaa414f1c9d04290d455f1e863e
Aleksandr Serenko <fafnur@yandex.ru>
2021-04-24 17:34:14 +0700

    Add load persons

--- src/app/booking/page/booking-page.module.ts
+++ src/app/booking/page/booking-page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { BuildingStateModule } from '@app/buildings/state';
+import { PersonStateModule } from '@app/persons/state';
 import { RoomServiceModule } from '@app/rooms/service';
 import { RoomStateModule } from '@app/rooms/state';
 
@@ -9,7 +10,7 @@ import { BookingPageComponent } from './booking-page.component';
 import { BookingPageRoutingModule } from './booking-page-routing.module';
 
 @NgModule({
-  imports: [CommonModule, BookingPageRoutingModule, RoomStateModule, BuildingStateModule, RoomServiceModule],
+  imports: [CommonModule, BookingPageRoutingModule, RoomStateModule, BuildingStateModule, PersonStateModule, RoomServiceModule],
   declarations: [BookingPageComponent],
 })
 export class BookingPageModule {}
--- src/app/persons/state/person-state.module.ts
+++ src/app/persons/state/person-state.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
-import { BuildingStorageModule } from '@app/buildings/storage';
+import { PersonStorageModule } from '@app/persons/storage';
 
 import { PersonEffects } from './person.effects';
 import { PersonFacade } from './person.facade';
 import { PERSON_FEATURE_KEY, reducer } from './person.reducer';
 
 @NgModule({
-  imports: [BuildingStorageModule, StoreModule.forFeature(PERSON_FEATURE_KEY, reducer), EffectsModule.forFeature([PersonEffects])],
+  imports: [PersonStorageModule, StoreModule.forFeature(PERSON_FEATURE_KEY, reducer), EffectsModule.forFeature([PersonEffects])],
   providers: [PersonFacade],
 })
 export class PersonStateModule {}
