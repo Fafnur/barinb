2e688928ec3a6ac4a6064590fb4bc8665122a3e3
Aleksandr Serenko <fafnur@yandex.ru>
2021-06-02 22:15:06 +0700

    Remove legacy

--- src/app/core/navigation/common/interfaces/navigation.interface.ts
+++ src/app/core/navigation/common/interfaces/navigation.interface.ts
@@ -4,17 +4,10 @@ export enum NavigationPath {
   Buildings = 'buildings',
   Persons = 'persons',
   RoomPage = 'rooms/:id',
-  /**
-   * NOTICE: Maybe it's need to remove
-   */
-  NotFoundPage = 'not-found',
   Admin = 'admin',
   AdminRoomsPage = 'admin/rooms',
-  AdminRoomPage = 'admin/rooms/:id',
   AdminBuildingsPage = 'admin/buildings',
-  AdminBuildingPage = 'admin/buildings/:id',
   AdminPersonsPage = 'admin/persons',
-  AdminPersonPage = 'admin/persons/:id',
 }
 
 export interface NavigationLink {
