61aaef52063b9a5f1d3334870e0491b46ebd3897
Aleksandr Serenko <fafnur@yandex.ru>
2021-06-03 22:06:03 +0700

    Change test for admin building page routing

--- src/app/buildings/admin-page/building-admin-page-routing.module.spec.ts
+++ src/app/buildings/admin-page/building-admin-page-routing.module.spec.ts
@@ -1,37 +1,41 @@
-import { TestBed } from '@angular/core/testing';
-import { MockComponents } from 'ng-mocks';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { MockModule } from 'ng-mocks';
 
-import { AdminDesktopMenuComponent, AdminLayoutComponent, AdminMobileMenuComponent } from '@app/admin/layout';
-import { ContainerComponent } from '@app/ui/container';
-import { ColumnComponent, RowComponent } from '@app/ui/grid';
+import { AdminLayoutComponent, AdminLayoutModule } from '@app/admin/layout';
+import { ContainerModule } from '@app/ui/container';
+import { GridModule } from '@app/ui/grid';
 
+import { AdminDesktopMenuModule } from '../../admin/layout/components/admin-desktop-menu/admin-desktop-menu.module';
+import { AdminMobileMenuModule } from '../../admin/layout/components/admin-mobile-menu/admin-mobile-menu.module';
 import { BuildingAdminPageComponent } from './building-admin-page.component';
 import { BuildingAdminPageRoutingModule } from './building-admin-page-routing.module';
-import { AdminBuildingsActionsComponent } from './components/admin-buildings-actions/admin-buildings-actions.component';
-import { AdminBuildingsListComponent } from './components/admin-buildings-list/admin-buildings-list.component';
-import { AdminBuildingsTableComponent } from './components/admin-buildings-table/admin-buildings-table.component';
+import { AdminBuildingsActionsModule } from './components/admin-buildings-actions/admin-buildings-actions.module';
+import { AdminBuildingsListModule } from './components/admin-buildings-list/admin-buildings-list.module';
+import { AdminBuildingsTableModule } from './components/admin-buildings-table/admin-buildings-table.module';
 
 describe('BuildingAdminPageRoutingModule', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BuildingAdminPageRoutingModule],
-      declarations: [
-        // TODO: Fix jest imports.
-        BuildingAdminPageComponent,
-        AdminLayoutComponent,
-        MockComponents(
-          RowComponent,
-          ColumnComponent,
-          AdminDesktopMenuComponent,
-          AdminMobileMenuComponent,
-          ContainerComponent,
-          AdminBuildingsActionsComponent,
-          AdminBuildingsTableComponent,
-          AdminBuildingsListComponent
-        ),
-      ],
-    }).compileComponents();
-  });
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          BuildingAdminPageRoutingModule,
+          MockModule(GridModule),
+          MockModule(AdminLayoutModule),
+          MockModule(ContainerModule),
+          MockModule(AdminBuildingsActionsModule),
+          MockModule(AdminBuildingsTableModule),
+          MockModule(AdminBuildingsListModule),
+          MockModule(AdminDesktopMenuModule),
+          MockModule(AdminMobileMenuModule),
+        ],
+        declarations: [
+          // TODO: Fix jest imports.
+          BuildingAdminPageComponent,
+          AdminLayoutComponent,
+        ],
+      }).compileComponents();
+    })
+  );
 
   it('should create', () => {
     expect(BuildingAdminPageRoutingModule).toBeTruthy();
