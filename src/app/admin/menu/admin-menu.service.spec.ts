f7c7c96181858a4297eb3e4aa94c95416ad4f41f
Aleksandr Serenko <fafnur@yandex.ru>
2021-06-03 13:34:13 +0700

    Add tests for admin menu service

--- src/app/admin/menu/admin-menu.service.spec.ts
+++ src/app/admin/menu/admin-menu.service.spec.ts
@@ -1,21 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-
-import { AdminMenuService } from './admin-menu.service';
+import { ADMIN_MENU_LINKS, AdminMenuService } from './admin-menu.service';
 
 describe('AdminMenuService', () => {
   let service: AdminMenuService;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      providers: [AdminMenuService],
-    }).compileComponents();
-
-    service = TestBed.inject(AdminMenuService);
+    service = new AdminMenuService();
   });
 
   it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create', () => {
+    expect(service.links()).toEqual(ADMIN_MENU_LINKS);
   });
 });
