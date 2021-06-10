fd051a20e8271d2b808dc4718e42bb17c289b178
Aleksandr Serenko <fafnur@yandex.ru>
2021-06-10 23:44:41 +0700

    Add tests for room service

--- src/app/rooms/service/room.service.spec.ts
+++ src/app/rooms/service/room.service.spec.ts
@@ -1,30 +1,63 @@
-import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { hot } from 'jasmine-marbles';
+import { ReplaySubject } from 'rxjs';
+import { anything, mock, when } from 'ts-mockito';
 
-import { RoomFacade } from '@app/rooms/state';
+import { providerOf } from '@app/core/testing';
+import { Room } from '@app/rooms/common';
+import { ROOM_STUB, RoomFacade, ROOMS_STUB } from '@app/rooms/state';
 
 import { RoomService } from './room.service';
 
 describe('RoomService', () => {
   let service: RoomService;
+  let roomFacadeMock: RoomFacade;
+  let rooms$: ReplaySubject<Room[]>;
+  let room$: ReplaySubject<Room>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [
-        RoomService,
-        {
-          provide: RoomFacade,
-          useValue: {
-            rooms$: of(),
-          } as Partial<RoomFacade>,
-        },
-      ],
-    }).compileComponents();
+  beforeEach(() => {
+    roomFacadeMock = mock(RoomFacade);
+
+    rooms$ = new ReplaySubject<Room[]>(1);
+    room$ = new ReplaySubject<Room>(1);
+  });
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        providers: [RoomService, providerOf(RoomFacade, roomFacadeMock)],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    when(roomFacadeMock.rooms$).thenReturn(rooms$);
+    when(roomFacadeMock.room$(anything())).thenReturn(room$);
 
     service = TestBed.inject(RoomService);
   });
 
   it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return rooms', () => {
+    rooms$.next(ROOMS_STUB);
+
+    const expected$ = hot('a', {
+      a: ROOMS_STUB,
+    });
+
+    expect(service.rooms$).toBeObservable(expected$);
+  });
+
+  it('should return room', () => {
+    room$.next(ROOM_STUB);
+
+    const expected$ = hot('a', {
+      a: ROOM_STUB,
+    });
+
+    expect(service.room$(anything())).toBeObservable(expected$);
   });
 });
