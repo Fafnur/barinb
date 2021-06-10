ec1e08a5632100e7cea367e417fbc039bae88779
Aleksandr Serenko <fafnur@yandex.ru>
2021-06-10 10:09:55 +0700

    Add tests for person pipes

--- src/app/persons/shared/person-full-name.pipe.spec.ts
+++ src/app/persons/shared/person-full-name.pipe.spec.ts
@@ -1,8 +1,19 @@
+import { PERSON_STUB } from '@app/persons/state';
+
 import { PersonFullNamePipe } from './person-full-name.pipe';
 
 describe('PersonFullNamePipe', () => {
+  let pipe: PersonFullNamePipe;
+
+  beforeEach(() => {
+    pipe = new PersonFullNamePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new PersonFullNamePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('create an instance', () => {
+    expect(pipe.transform(PERSON_STUB)).toBe(`${PERSON_STUB.firstName} ${PERSON_STUB.lastName}`);
   });
 });
