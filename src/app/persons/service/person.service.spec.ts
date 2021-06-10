8b30408b3739665defa823e0a6b9a05df57d0aa6
Aleksandr Serenko <fafnur@yandex.ru>
2021-06-10 10:16:06 +0700

    Add tests for person service

--- src/app/persons/service/person.service.spec.ts
+++ src/app/persons/service/person.service.spec.ts
@@ -1,30 +1,63 @@
-import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { hot } from 'jasmine-marbles';
+import { ReplaySubject } from 'rxjs';
+import { anything, mock, when } from 'ts-mockito';
 
-import { PersonFacade } from '@app/persons/state';
+import { providerOf } from '@app/core/testing';
+import { Person } from '@app/persons/common';
+import { PERSON_STUB, PersonFacade, PERSONS_STUB } from '@app/persons/state';
 
 import { PersonService } from './person.service';
 
 describe('PersonService', () => {
   let service: PersonService;
+  let personFacadeMock: PersonFacade;
+  let persons$: ReplaySubject<Person[]>;
+  let person$: ReplaySubject<Person>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [
-        PersonService,
-        {
-          provide: PersonFacade,
-          useValue: {
-            persons$: of(),
-          } as Partial<PersonFacade>,
-        },
-      ],
-    }).compileComponents();
+  beforeEach(() => {
+    personFacadeMock = mock(PersonFacade);
+
+    persons$ = new ReplaySubject<Person[]>(1);
+    person$ = new ReplaySubject<Person>(1);
+  });
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        providers: [PersonService, providerOf(PersonFacade, personFacadeMock)],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    when(personFacadeMock.persons$).thenReturn(persons$);
+    when(personFacadeMock.person$(anything())).thenReturn(person$);
 
     service = TestBed.inject(PersonService);
   });
 
   it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return persons', () => {
+    persons$.next(PERSONS_STUB);
+
+    const expected$ = hot('a', {
+      a: PERSONS_STUB,
+    });
+
+    expect(service.persons$).toBeObservable(expected$);
+  });
+
+  it('should return person', () => {
+    person$.next(PERSON_STUB);
+
+    const expected$ = hot('a', {
+      a: PERSON_STUB,
+    });
+
+    expect(service.person$(anything())).toBeObservable(expected$);
   });
 });
